dcd68590a697ba8d7337cb073eb366eaf8872a98
pylover <vahid.mardani@gmail.com>
2020-02-26 14:31:02 +0330

    Multipart form test

--- bootstrapper_test.go
+++ bootstrapper_test.go
@@ -36,7 +36,7 @@ func TestSimpleRequest(t *testing.T) {
 	assert.Equal(t, body, "Hello")
 }
 
-func TestPost(t *testing.T) {
+func TestURLEncoded(t *testing.T) {
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		fmt.Fprintf(w, "%s %s", r.FormValue("foo"), r.FormValue("baz"))
@@ -50,6 +50,38 @@ Content-Length: 15
 Content-Type: application/x-www-form-urlencoded
 
 foo=bar&baz=qux`)
+
+	assert.Equal(t, "bar qux", body)
+}
+
+func TestMultipart(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Here r is *http.Request
+		parseErr := r.ParseMultipartForm(1024 * 10000) // maxMemory 32MB
+		if parseErr != nil {
+			http.Error(w, "failed to parse multipart message", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "%s %s", r.FormValue("foo"), r.FormValue("baz"))
+	})
+
+	body := sendRequest(t, handler, `POST / HTTP/1.1
+Host: example.org
+User-Agent: curl/7.58.0
+Accept: */*
+Content-Length: 236
+Content-Type: multipart/form-data; boundary=------------------------1b9c1df10c236cdc
+
+--------------------------1b9c1df10c236cdc
+Content-Disposition: form-data; name="foo"
+
+bar
+--------------------------1b9c1df10c236cdc
+Content-Disposition: form-data; name="baz"
+
+qux
+--------------------------1b9c1df10c236cdc--
+`)
 
 	assert.Equal(t, "bar qux", body)
 }
