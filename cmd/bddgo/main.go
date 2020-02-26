f55f55d8eec91fab6791063dd94b0cec456d3fc7
pylover <vahid.mardani@gmail.com>
2020-02-26 20:38:49 +0330

    CLI subcommands

--- cmd/bddgo/main.go
+++ cmd/bddgo/main.go
@@ -10,13 +10,43 @@ import (
 
 func parseArguments(packageName *string, functionName *string) {
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [packages]\n", os.Args[0])
+		fmt.Fprintf(
+			flag.CommandLine.Output(),
+			"Usage: %s {init|test|load}\n",
+			os.Args[0],
+		)
 		flag.PrintDefaults()
 	}
 
-	flag.StringVar(functionName, "function", "GetMainHandler",
+	initCommand := flag.NewFlagSet("init", flag.ExitOnError)
+	loadCommand := flag.NewFlagSet("load", flag.ExitOnError)
+	loadCommand.StringVar(functionName, "function", "GetMainHandler",
 		"Function name to load and call to get the main handler.")
-	flag.Parse()
+	loadCommand.Usage = func() {
+		fmt.Fprintf(
+			flag.CommandLine.Output(),
+			"Usage: %s [packages]\n",
+			os.Args[0],
+		)
+		flag.PrintDefaults()
+	}
+
+	if len(os.Args) <= 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "init":
+		initCommand.Parse(os.Args[2:])
+
+	case "load":
+		loadCommand.Parse(os.Args[2:])
+
+	default:
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	if flag.NArg() > 0 {
 		*packageName = flag.Args()[0]
